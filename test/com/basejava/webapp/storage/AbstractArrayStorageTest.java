1925a242b6edc7a4805cb9055825998e8d4dd2ab
MrAntoniS <0shkiper0@gmail.com>
2020-05-20 14:01:39 +0300

    Lesson_6

--- test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
@@ -1,105 +1,23 @@
 package com.basejava.webapp.storage;
 
-import com.basejava.webapp.exception.ExistStorageException;
-import com.basejava.webapp.exception.NotExistStorageException;
 import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static com.basejava.webapp.storage.AbstractArrayStorage.STORAGE_LIMIT;
-import static org.junit.Assert.assertEquals;
 
-public abstract class AbstractArrayStorageTest {
-    private Storage storage;
-
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
 
     public AbstractArrayStorageTest(Storage storage) {
-        this.storage = storage;
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        storage.clear();
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_2));
-        storage.save(new Resume(UUID_3));
-    }
-
-    @Test
-    public void size() throws Exception {
-        assertEquals(3, storage.size());
-    }
-
-    @Test
-    public void clear() throws Exception {
-        storage.clear();
-        assertEquals(0, storage.size());
-    }
-
-    @Test
-    public void update() throws Exception {
-        assertEquals(new Resume(UUID_1), storage.get(UUID_1));
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void updateNotExist() throws Exception {
-        Resume testResume = new Resume("dummy");
-        storage.update(testResume);
-    }
-
-    @Test
-    public void getAll() throws Exception {
-        Resume[] actualResume = storage.getAll();
-        assertEquals(actualResume, storage.getAll());
-    }
-
-    @Test
-    public void save() throws Exception {
-        storage.clear();
-        storage.save(new Resume(UUID_1));
-        assertEquals(1, storage.size());
-    }
-
-    @Test(expected = ExistStorageException.class)
-    public void saveExist() throws Exception {
-        storage.save(new Resume(UUID_1));
+        super(storage);
     }
 
     @Test(expected = StorageException.class)
-    public void saveFullStorage() throws Exception {
+    public void saveOverflow() throws Exception {
         int i = 0;
         for (i = 3; i < STORAGE_LIMIT; i++) {
             storage.save(new Resume("UUID_" + i));
         }
         storage.save(new Resume("UUID_" + i));
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void delete() throws Exception {
-        storage.delete(UUID_1);
-        assertEquals(2, storage.size());
-        storage.get(UUID_1);
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void deleteNotExist() throws Exception {
-        storage.delete("dummy");
-    }
-
-    @Test
-    public void get() throws Exception {
-        assertEquals(new Resume(UUID_1), storage.get(UUID_1));
-    }
-
-    @Test(expected = NotExistStorageException.class)
-    public void getNotExist() throws Exception {
-        storage.get("dummy");
     }
 }
--- test/com/basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -9,11 +9,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AbstractStorageTest {
-    private Storage storage;
+    protected Storage storage;
 
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
+    protected static final String UUID_1 = "uuid1";
+    protected static final String UUID_2 = "uuid2";
+    protected static final String UUID_3 = "uuid3";
 
     public AbstractStorageTest(Storage storage) {
         this.storage = storage;
