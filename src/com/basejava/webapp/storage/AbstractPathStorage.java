fc1d1b83006d927fb7c78fd316382c7397d76276
MrAntoniS <0shkiper0@gmail.com>
2020-06-14 17:06:48 +0300

    Create ObjectStreamPathStorage, ObjectStreamPathStorageTest

--- src/com/basejava/webapp/storage/AbstractPathStorage.java
+++ src/com/basejava/webapp/storage/AbstractPathStorage.java
@@ -1,15 +1,24 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public abstract class AbstractPathStorage extends AbstractStorage<Path> {
     private Path directory;
+
+    protected abstract Resume doRead(InputStream is) throws IOException;
+
+    protected abstract void doWrite(Resume r, OutputStream os) throws IOException;
 
     protected AbstractPathStorage(String dir) {
         directory = Paths.get(dir);
@@ -21,46 +30,83 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
 
     @Override
     protected Path getSearchKey(String uuid) {
-        return null;
+        return directory.resolve(uuid);
     }
 
     @Override
-    protected void doUpdate(Resume r, Path searchKey) {
-
+    protected void doUpdate(Resume r, Path path) {
+        try {
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(String.valueOf(path))));
+        } catch (IOException e) {
+            throw new StorageException("File write error", path.getFileName().toString(), e);
+        }
     }
 
     @Override
-    protected boolean isExist(Path searchKey) {
-        return false;
+    protected boolean isExist(Path path) {
+        try {
+            return Files.list(directory).anyMatch(path::equals);
+        } catch (IOException e) {
+            throw new StorageException("File not exist", path.getFileName().toString(), e);
+        }
     }
 
     @Override
-    protected void doSave(Resume r, Path searchKey) {
-
+    protected void doSave(Resume r, Path path) {
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new StorageException("File save error", path.getFileName().toString(), e);
+        }
+        doUpdate(r, path);
     }
 
     @Override
-    protected Resume doGet(Path searchKey) {
-        return null;
+    protected Resume doGet(Path path) {
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(new File(String.valueOf(path)))));
+        } catch (IOException e) {
+            throw new StorageException("File read error", path.getFileName().toString(), e);
+        }
     }
 
     @Override
-    protected void doDelete(Path searchKey) {
-
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("File delete error", path.getFileName().toString(), e);
+        }
     }
 
     @Override
     protected List<Resume> doCopyAll() {
-        return null;
+        try {
+            List<Resume> resumeStorage = new ArrayList<>();
+            Files.list(directory).forEach(path -> {
+                resumeStorage.add(doGet(path));
+            });
+            return resumeStorage;
+        } catch (IOException e) {
+            throw new StorageException("Directory read error", null);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        try {
+            return (int) Files.list(directory).count();
+        } catch (IOException e) {
+            throw new StorageException("Directory read error", null);
+        }
     }
 
     @Override
     public void clear() {
-
+        try {
+            Files.list(directory).forEach(this::doDelete);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", null);
+        }
     }
 }
--- test/com/basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,7 +17,8 @@ import static org.junit.Assert.*;
 
 public abstract class AbstractStorageTest {
 
-    protected static final File STORAGE_DIR = new File("C:\\Users\\Антон\\Desktop\\basejava\\basejava\\storage");
+    protected static final String DIR = "C:\\Users\\Антон\\Desktop\\basejava\\basejava\\storage";
+    protected static final File STORAGE_DIR = new File(DIR);
     protected Storage storage;
 
     protected static final String UUID_1 = "uuid1";
