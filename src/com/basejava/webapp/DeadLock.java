023709999f775cf0705a6ddda36304c81870496e
MrAntoniS <0shkiper0@gmail.com>
2020-07-13 19:34:31 +0300

    Lesson 12. HW11 analysis.

--- src/com/basejava/webapp/DeadLock.java
+++ src/com/basejava/webapp/DeadLock.java
@@ -1,33 +1,28 @@
 package com.basejava.webapp;
 
 public class DeadLock {
-    private static final Object LOCK_1 = new Object();
-    private static final Object LOCK_2 = new Object();
+    private static final String LOCK_1 = "Lock_1";
+    private static final String LOCK_2 = "Lock_2";
 
     public static void main(String[] args) {
-        Thread tread0 = new Thread(() -> {
-            takeObject(LOCK_1, LOCK_2);
-        });
-
-        Thread tread1 = new Thread(() -> {
-            takeObject(LOCK_2, LOCK_1);
-        });
-
-        tread0.start();
-        tread1.start();
+        takeObject(LOCK_1, LOCK_2);
+        takeObject(LOCK_2, LOCK_1);
     }
 
     public static void takeObject(Object obj1, Object obj2) {
-        synchronized (obj1) {
-            System.out.println("Потоком " + Thread.currentThread().getName() + " захвачен обьект " + obj1);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        new Thread(() -> {
+            synchronized (obj1) {
+                System.out.println("Потоком " + Thread.currentThread().getName() + " захвачен обьект " + obj1);
+                try {
+                    Thread.sleep(500);
+                    System.out.println("Поток " + Thread.currentThread().getName() + " ожидает обьект " + obj2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (obj2) {
+                    System.out.println("Потоком " + Thread.currentThread().getName() + " захвачен обьект " + obj2);
+                }
             }
-            synchronized (obj2) {
-                System.out.println("Потоком " + Thread.currentThread().getName() + " захвачен обьект " + obj2);
-            }
-        }
+        }).start();
     }
 }
