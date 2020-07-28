03ef3ae810ff6a039c28a98d242f4861245764ba
MrAntoniS <0shkiper0@gmail.com>
2020-07-28 22:43:43 +0300

    Lesson 13.01 init_db.sql

--- src/com/basejava/webapp/MainStream.java
+++ src/com/basejava/webapp/MainStream.java
@@ -2,12 +2,13 @@ package com.basejava.webapp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class MainStream {
 
     static int[] values = {2, 1, 9, 2, 9, 1, 1};
-    static Integer[] integers = {2, 7, 9, 2, 8, 1, 4};
+    static Integer[] integers = {2, 7, 9, 2, 8, 1, 4, 1};
 
     public static void main(String[] args) {
         System.out.println(minValue(values));
@@ -19,12 +20,15 @@ public class MainStream {
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        return integers.stream().mapToInt(Integer::intValue).filter(s -> {
-            int sum = 0;
-            for (Integer integer : integers) {
-                sum += integer;
-            }
-            return s % 2 == (sum % 2 == 0 ? 0 : 1);
-        }).boxed().collect(Collectors.toList());
+        int sum = integers.stream().reduce(0, Integer::sum);
+        Predicate<Integer> predicate = s -> (sum % 2 == 0) == (s % 2 == 0);
+        return integers.stream().filter(predicate).collect(Collectors.toList());
+//        return integers.stream().mapToInt(Integer::intValue).filter(s -> {
+//            int sum = 0;
+//            for (Integer integer : integers) {
+//                sum += integer;
+//            }
+//            return s % 2 == (sum % 2 == 0 ? 0 : 1);
+//        }).boxed().collect(Collectors.toList());
     }
 }
